77722ee2a301fcbe709c222d82d4b5ee782a51d4
Ivan <calintz1@hotmail.com>
2021-03-28 12:12:28 -0300

    Update
    Criado About component, Body component, experience component.
    
    Ajustes Header Component e profile module.

--- portfolio/src/app/components/profile/profile.module.ts
+++ portfolio/src/app/components/profile/profile.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BodyComponent, AboutComponent],
+  declarations: [
+    HeaderComponent,
+    BodyComponent,
+    AboutComponent,
+    ExperienceComponent,
+  ],
   exports: [HeaderComponent, BodyComponent],
-  imports: [CommonModule, MatTabsModule],
+  imports: [CommonModule, MatTabsModule, MatButtonModule, ScrollingModule],
 })
 export class ProfileModule {}

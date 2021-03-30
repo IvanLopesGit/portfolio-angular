925e7779a1e07f4f766125facc823ed578db6cc4
Ivan <calintz1@hotmail.com>
2021-03-30 10:13:00 -0300

    Update
    Updtate body component, criado certificados component, educação, experiência, skills

--- portfolio/src/app/components/profile/profile.module.ts
+++ portfolio/src/app/components/profile/profile.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { CertificatesComponent } from './certificates/certificates.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +19,12 @@ import { ExperienceComponent } from './experience/experience.component';
     BodyComponent,
     AboutComponent,
     ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    CertificatesComponent,
   ],
   exports: [HeaderComponent, BodyComponent],
-  imports: [CommonModule, MatTabsModule, MatButtonModule, ScrollingModule],
+  imports: [CommonModule, MatTabsModule, MatButtonModule, MatDividerModule],
 })
 export class ProfileModule {}
